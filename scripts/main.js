a8655a8c8f329ccdc164896cbd9e9fcbf1ed5e4c
Ni236630 <nicholas.douso@gmail.com>
2020-11-22 21:32:12 -0600

    could not remember what i updated

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,13 @@
 import { useBusiness } from "./BusinessProvider.js";
 import { businessList } from "./BusinessList.js";
 import { businessListNY } from "./BusinessListNY.js";
+import { businessListManufacturing } from "./BusinessListManufacturer.js";
+import { useAgents } from "./purchasingAgents.js";
 
+useAgents()
 businessList()
 businessListNY()
+businessListManufacturing()
 
 // test to make sure that the list of business is being returned
 const businessOrder = useBusiness()
