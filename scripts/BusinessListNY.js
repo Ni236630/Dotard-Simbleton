fbb65caa28de4ffd4869318e491c9d8c8dbb6d4e
Ni236630 <nicholas.douso@gmail.com>
2020-11-24 15:46:49 -0600

    finally got search function to work; watch for camelCase

--- scripts/BusinessListNY.js
+++ scripts/BusinessListNY.js
@@ -4,13 +4,22 @@ import { business } from "./Business.js";
 const contentTarget = document.querySelector(".businessList--newYork")
 
 export const businessListNY = () => {
-  const businessArray = useBusiness()
+  //const businessArray = useBusiness()
   contentTarget.innerHTML = "<h1>New York Businesses</h1>"
   
-  businessArray.filter(
-    function(businessObject){
-    if (businessObject.addressStateCode === "NY"){
-      return contentTarget.innerHTML += business(businessObject)}
+  // useBusiness().filter(
+  //   function(businessObject){
+  //   if (businessObject.addressStateCode === "NY"){
+  //     return contentTarget.innerHTML += business(businessObject)}
+  //   }
+  // );
+  
+  useBusiness().forEach(
+    (businessObject) => {
+      if (businessObject.addressStateCode === "NY"){
+        return  contentTarget.innerHTML += business(businessObject)
+      }
+     
     }
   );
 }
